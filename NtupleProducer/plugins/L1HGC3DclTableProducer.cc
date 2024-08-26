57877aeac24697411cce3e5fa335edf098fa731b
Kyungmin Park <kyungmin.park@cern.ch>
2024-08-26 20:08:18 +0200

    Re-update the HGC table producer

--- NtupleProducer/plugins/L1HGC3DclTableProducer.cc
+++ NtupleProducer/plugins/L1HGC3DclTableProducer.cc
@@ -8,6 +8,7 @@
 
 #include "DataFormats/L1THGCal/interface/HGCalMulticluster.h"
 #include "L1Trigger/Phase2L1ParticleFlow/interface/HGC3DClusterEgID.h"
+#include "L1Trigger/Phase2L1ParticleFlow/interface/HGC3DClusterID.h"
 
 #include "DataFormats/Math/interface/deltaR.h"
 
@@ -36,7 +37,8 @@ class L1HGC3DclTableProducer : public edm::stream::EDProducer<>  {
         StringCutObjectSelector<l1t::HGCalMulticluster> sel_;
         l1tpf::HGC3DClusterEgID emVsPionID_;
         l1tpf::HGC3DClusterEgID emVsPUID_;
-        std::unique_ptr<HGCalTriggerClusterIdentificationBase> id_;
+        l1tpf::HGC3DClusterID multiClassPID_;
+   	std::unique_ptr<HGCalTriggerClusterIdentificationBase> id_;
 };
 
 L1HGC3DclTableProducer::L1HGC3DclTableProducer(const edm::ParameterSet& iConfig) :
@@ -44,7 +46,8 @@ L1HGC3DclTableProducer::L1HGC3DclTableProducer(const edm::ParameterSet& iConfig)
     clusters_(consumes<l1t::HGCalMulticlusterBxCollection>(iConfig.getParameter<edm::InputTag>("src"))),
     sel_(iConfig.getParameter<std::string>("cut"), true),
     emVsPionID_(iConfig.getParameter<edm::ParameterSet>("emVsPionID")),
-    emVsPUID_(iConfig.getParameter<edm::ParameterSet>("emVsPUID")) 
+    emVsPUID_(iConfig.getParameter<edm::ParameterSet>("emVsPUID")),
+    multiClassPID_(iConfig.getParameter<edm::ParameterSet>("multiClassPID"))	
 {
     produces<nanoaod::FlatTable>();
 
@@ -82,6 +85,7 @@ L1HGC3DclTableProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetu
     unsigned int ncands = selected.size();
     auto out = std::make_unique<nanoaod::FlatTable>(ncands, name_, false, true);
 
+    // Binary classifier legacy BDTs
     std::vector<float> vals_puid, vals_pfemid, vals_egemid;
     std::vector<bool> pass_puid, pass_pfemid; 
     vals_puid.resize(ncands);
@@ -90,10 +94,19 @@ L1HGC3DclTableProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetu
     pass_puid.resize(ncands); 
     pass_pfemid.resize(ncands);
 
+    // Multi-classifier BDT
+    std::vector<float> vals_multiClassMaxScore, vals_multiClassPUid, vals_multiClassPionid, vals_multiClassEmid;
+
+    vals_multiClassMaxScore.resize(ncands);
+    vals_multiClassPUid.resize(ncands);
+    vals_multiClassPionid.resize(ncands);
+    vals_multiClassEmid.resize(ncands);
+
     for (unsigned int i = 0; i < ncands; ++i) {
         auto cl3d = *selected[i];
 
         l1t::PFCluster cluster;
+	// Binary classifier Legacy BDTs
         bool passEmVsPU = false;
         bool passPFEmVsPion = false;
         if (!emVsPUID_.method().empty()) {
@@ -108,6 +121,15 @@ L1HGC3DclTableProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetu
         vals_egemid[i] = id_->value(cl3d);
         pass_puid[i] = passEmVsPU;
         pass_pfemid[i] = passPFEmVsPion;
+
+	// Multi-class BDT
+	float maxScore = multiClassPID_.evaluate(cl3d, cluster);
+
+        vals_multiClassMaxScore[i] = maxScore;
+        vals_multiClassPUid[i] = cluster.puIDScore();
+ 	vals_multiClassPionid[i] = cluster.piIDScore();
+	vals_multiClassEmid[i] = cluster.emIDScore();
+
     }
     out->addColumn<float>("pfPuIdScore", vals_puid, "");
     out->addColumn<float>("pfEmIdScore", vals_pfemid, "");
@@ -115,10 +137,13 @@ L1HGC3DclTableProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetu
     out->addColumn<bool>("pfPuIdPass", pass_puid, "");
     out->addColumn<bool>("pfEmIdPass", pass_pfemid, "");
 
+    out->addColumn<float>("multiClassMaxScore", vals_multiClassMaxScore, ""); 
+    out->addColumn<float>("multiClassPuIdScore", vals_multiClassPUid, ""); 
+    out->addColumn<float>("multiClassPionIdScore", vals_multiClassPionid, ""); 
+    out->addColumn<float>("multiClassEmIdScore", vals_multiClassEmid, ""); 
+
     // save to the event branches
     iEvent.put(std::move(out));
-
-   
 }
 
 //define this as a plug-in
